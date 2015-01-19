b26d1f63b49b0cfc0f8f62b8f5037370606f82d5
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-01-19 00:43:51 -0500

    cm.mk: Add beanstalk vendor

--- cm.mk
+++ cm.mk
@@ -1,10 +1,10 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/beanstalk/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/beanstalk/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/t6dwg/full_t6dwg.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htccn_chs_ct BUILD_ID=JSS15J BUILD_FINGERPRINT="htc/htccn_chs_ct/t6dwg:4.3/JSS15J/282597.1:user/release-keys" PRIVATE_BUILD_DESC="1.41.1401.1 CL282597 release-keys"
 
-PRODUCT_NAME := cm_t6dwg
+PRODUCT_NAME := bs_t6dwg
 PRODUCT_DEVICE := t6dwg
